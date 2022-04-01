9bf123bf3bc371f7509dd26b997cc419e47a39e0
jungguji <junggu.ji.dev@gmail.com>
2022-04-01 23:59:49 +0900

    feat : 컨버터 만드는 중

--- src/main/java/com/jgji/testcode/Converter.java
+++ src/main/java/com/jgji/testcode/Converter.java
@@ -3,6 +3,7 @@ package com.jgji.testcode;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Converter {
@@ -16,17 +17,29 @@ public class Converter {
     private static final String EMPTY = "";
 
     private static final String UNDERSCORE = "_";
+    private static final String ENTER = "\n";
+
 
     private static final int ASCII = 32;
 
-
-
-
-    public String convert(String insertQuery) {
+    public String convert(String tableName, String insertQuery) {
         String[] columnAndValue = StringUtils.substringsBetween(insertQuery, OPEN, END);
 
         List<String> columns = getColumns(columnAndValue[0]);
-        return EMPTY;
+        List<String> variables = convertVariables(columns);
+
+        List<String> values = values(columnAndValue[1]);
+
+        String camelcaseTableName = convertVariables(Arrays.asList(tableName)).get(0);
+        StringBuilder code = new StringBuilder();
+
+        code.append(camelcaseTableName).append("builder()").append(ENTER);
+        for (int i = 0; i < variables.size(); i++) {
+            code.append(variables.get(i)).append(OPEN).append(values.get(i)).append(END).append(ENTER);
+        }
+        code.append(".build();");
+
+        return code.toString();
     }
 
     public List<String> getColumns(String args) {
@@ -50,12 +63,7 @@ public class Converter {
             if (column.contains(UNDERSCORE)) {
                 String[] splits = column.split(UNDERSCORE);
 
-                char[] chars = splits[1].toCharArray();
-
-                char ch = (char) (chars[0] - ASCII);
-                chars[0] = ch;
-
-                String camelcase = new String(chars);
+                String camelcase = convertCamelcase(splits);
 
                 variables.add(splits[0] + camelcase);
             } else {
@@ -64,5 +72,46 @@ public class Converter {
         }
 
         return variables;
+    }
+
+    private String convertCamelcase(String[] splits) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < splits.length; i++) {
+            String split = splits[i];
+
+            char[] chars = split.toCharArray();
+            char ch = (char) (chars[0] - ASCII);
+            chars[0] = ch;
+
+            sb.append(chars);
+        }
+
+        return sb.toString();
+    }
+
+    public List<String> values(String args) {
+        final char separator = ',';
+        char[] all = args.toCharArray();
+
+        List<String> values = new ArrayList<>();
+
+        StringBuilder value = new StringBuilder();
+
+        boolean open = false;
+        boolean end = false;
+
+        for (char ch : all) {
+            if (ch == separator) {
+                values.add(value.toString());
+                value = new StringBuilder();
+                open = false;
+                end = false;
+            } else {
+                value.append(ch);
+            }
+        }
+
+        return values;
     }
 }
--- src/test/java/com/jgji/testcode/ConverterTest.java
+++ src/test/java/com/jgji/testcode/ConverterTest.java
@@ -18,6 +18,20 @@ class ConverterTest {
 
     @Test
     void convert() {
+        //given
+        String query = "INSERT INTO partner (created_at,updated_at,name,address,detail_address,homepage,description,image,opening_hours,ending_hours,phone,tab_category_id,strict_company_id,other_cost,fix_order,region,holiday,etc,registration_number) VALUES (NULL,'2022-01-21 09:26:59','더채플앳청담','서울 강남구 선릉로 757','','http://www.thechapel.co.kr','세상에 없던 가장 아름다운 웨딩의 대명사, 더채플 모두가 인정하는 격조 높은 클래스는 웨딩, 본연의 가치에 집중하는 것에서 출발해 상상 그 이상의 환희를 선사하기에 더욱 특별합니다.\n" +
+                "\n" +
+                " 여유롭고 이국적인 휴양지의 설렘과 자연에서 영감을 얻은 싱그러운 분위기, 우아하고 로맨틱한 예식을 도심 속에서도 프라이빗하게 만끽할 수 있는 진정한 한국형 채플 웨딩.\n" +
+                "\n" +
+                "국내 최고 높이의 천장고 아래 펼쳐지는 드라마틱한 버진로드와 압도적인 웅장함, 곳곳에 더해진 사랑스러운 축복의 메시지까지 모두의 축복 속에 완성되는 위대한 러브스토리의 절정,\n" +
+                "\n" +
+                "더채플 웨딩의 주인공이 되어주세요.','https://s3-ap-northeast-2.amazonaws.com/wedqueen/admin/wedding/company/thumbnail/156378515778986FF44BA9B470A1C079B204E7DF9EA20.png','3000-01-31 10:00:01','3000-01-31 19:00:01','02-421-1121',102,NULL,NULL,NULL,'seocho','설연휴, 추석연휴','건물 내 200대, 외부 300대','');";
+        String tableName = "partner";
+        //when
+        String when = converter.convert(tableName, query);
+        //then
+
+        log.info(when);
     }
 
     @Test
@@ -50,13 +64,48 @@ class ConverterTest {
     @Test
     void convertVariables() {
         List<String> given = Arrays.asList(
-                "id"
+                "test_q_tdf_gsg"
                 , "user_name"
-                , "user_age"
+                , "user_id"
+                , "id"
         );
 
         List<String> whens = converter.convertVariables(given);
 
-        assertThat(whens).contains("userName", "userAge", "id");
+        assertThat(whens).contains("testQTdfGsg", "userName", "userId", "id");
+    }
+
+    @Test
+    void betweenTest() {
+        //given
+        String args = "NULL,'2022-01-21 09:26:59','더채플앳청담','서울 강남구 선릉로 757','','http://www.thechapel.co.kr','세상에 없던 가장 아름다운 웨딩의 대명사, 더채플 모두가 인정하는 격조 높은 클래스는 웨딩, 본연의 가치에 집중하는 것에서 출발해 상상 그 이상의 환희를 선사하기에 더욱 특별합니다.\n" +
+                "\n" +
+                " 여유롭고 이국적인 휴양지의 설렘과 자연에서 영감을 얻은 싱그러운 분위기, 우아하고 로맨틱한 예식을 도심 속에서도 프라이빗하게 만끽할 수 있는 진정한 한국형 채플 웨딩.\n" +
+                "\n" +
+                "국내 최고 높이의 천장고 아래 펼쳐지는 드라마틱한 버진로드와 압도적인 웅장함, 곳곳에 더해진 사랑스러운 축복의 메시지까지 모두의 축복 속에 완성되는 위대한 러브스토리의 절정,\n" +
+                "\n" +
+                "더채플 웨딩의 주인공이 되어주세요.','https://s3-ap-northeast-2.amazonaws.com/wedqueen/admin/wedding/company/thumbnail/156378515778986FF44BA9B470A1C079B204E7DF9EA20.png','3000-01-31 10:00:01','3000-01-31 19:00:01','02-421-1121',102,NULL,NULL,NULL,'seocho','설연휴, 추석연휴','건물 내 200대, 외부 300대',''";
+        String[] whens = StringUtils.substringsBetween(args, ",", ",");
+
+        for (String when : whens) {
+            System.out.println("when = " + when);
+        }
+    }
+
+    @Test
+    void values() {
+        String args = "NULL,'2022-01-21 09:26:59','더채플앳청담','서울 강남구 선릉로 757','','http://www.thechapel.co.kr','세상에 없던 가장 아름다운 웨딩의 대명사, 더채플 모두가 인정하는 격조 높은 클래스는 웨딩, 본연의 가치에 집중하는 것에서 출발해 상상 그 이상의 환희를 선사하기에 더욱 특별합니다.\n" +
+                "\n" +
+                " 여유롭고 이국적인 휴양지의 설렘과 자연에서 영감을 얻은 싱그러운 분위기, 우아하고 로맨틱한 예식을 도심 속에서도 프라이빗하게 만끽할 수 있는 진정한 한국형 채플 웨딩.\n" +
+                "\n" +
+                "국내 최고 높이의 천장고 아래 펼쳐지는 드라마틱한 버진로드와 압도적인 웅장함, 곳곳에 더해진 사랑스러운 축복의 메시지까지 모두의 축복 속에 완성되는 위대한 러브스토리의 절정,\n" +
+                "\n" +
+                "더채플 웨딩의 주인공이 되어주세요.','https://s3-ap-northeast-2.amazonaws.com/wedqueen/admin/wedding/company/thumbnail/156378515778986FF44BA9B470A1C079B204E7DF9EA20.png','3000-01-31 10:00:01','3000-01-31 19:00:01','02-421-1121',102,NULL,NULL,NULL,'seocho','설연휴, 추석연휴','건물 내 200대, 외부 300대',''";
+
+        List<String> whens = converter.values(args);
+
+        for (String when : whens) {
+            log.info(when);
+        }
     }
 }
